2ea63ac9fbc7d346e9a5f1fb11f2474c9d33cfe3
Anderssen Hutama <kylokyo3@gmail.com>
2023-12-13 00:36:57 +0700

    add loading

--- client/src/home/OtherProducts.jsx
+++ client/src/home/OtherProducts.jsx
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from "react";
 import ProductCards from "../components/ProductCards";
+import {Circles} from 'react-loading-icons';
 
 const OtherProducts = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_LocalHost}/all-products`)
       .then((res) => res.json())
-      .then((data) => setProducts(data.slice(0, 6)));
+      .then((data) => {
+        setProducts(data.slice(0, 6));
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
   }, []);
   return (
     <div>
-      <ProductCards products={products} headline="" />
+        {loading ? (
+        // Show a loading spinner or message while fetching data
+        <div className="flex items-center justify-center">
+    
+          <Circles stroke="#000" strokeOpacity={1} speed={.75} className="mr-5"/>
+          Loading...        
+          </div>
+          
+      ) : (
+        // Render the product cards when data is available
+        <ProductCards products={products} headline="" />
+      )}
     </div>
   );
 };
